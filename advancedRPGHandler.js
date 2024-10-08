c1cbafbc106b75d46c3daf022ff805ec1d051ec0
SUCC-u-lent <william.i.ney@gmail.com>
2024-10-08 22:10:24 +1000

    Commited Change

--- advancedRPGHandler.js
+++ advancedRPGHandler.js
@@ -5,6 +5,6 @@ export async function addDropdown(){
     const element = await $.get(`${extensionFolderPath}/advancedTabRPG.html`);
     const element2 = await $.get(`${extensionFolderPath}/rpgEntry.html`);
     $("#character_popup").children().eq(4).after(element)
-    element.append(element2)
+    $("#rpg_data_content").append(element2)
     print("Inserted advanced")
 }
