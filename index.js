972a4a87e7316ca9a5e85dfa5c2596167f4a7a97
SUCC-u-lent <william.i.ney@gmail.com>
2024-10-08 23:08:21 +1000

    Commited Change

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const extensionName = "st-extension-rpg";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 
 import { addDropdown } from "./advancedRPGHandler.js"
-import {init} from "./attributeHandler.js"
 
 jQuery(()=>{
     addDropdown()
