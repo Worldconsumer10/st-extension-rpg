1129cee26a1f3d6820bf2c881e182b28a6af0de2
SUCC-u-lent <william.i.ney@gmail.com>
2024-10-08 23:07:23 +1000

    Commited Change

--- index.js
+++ index.js
@@ -6,5 +6,20 @@ import {init} from "./attributeHandler.js"
 
 jQuery(()=>{
     addDropdown()
-    init()
+    $('#advanced_rpg_add_element').on('click', function() {
+        const template = $('#entryTemplate');
+        const newEntry = template.clone().show(); // Clone and show the entry
+        newEntry.find('.removeEntry').on('click', function() {
+            newEntry.remove(); // Remove the entry when the remove button is clicked
+        });
+
+        // Add input listener to only allow numbers in the max_value field
+        newEntry.find('.max_value').on('input', function() {
+            // Get the current value and replace non-digit characters
+            const value = $(this).val().replace(/\D/g, '');
+            $(this).val(value); // Set the filtered value back to the input
+        });
+
+        $('#entriesContainer').append(newEntry); // Append the new entry
+    });
 })
