a84936042f0acc6f19c48b332a494c9e262a7072
SUCC-u-lent <william.i.ney@gmail.com>
2024-10-09 19:39:02 +1000

    Commited Change

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function addAttributes(){
         const element = messages[i];
         const name = $(this).find('.name_text')
         console.log(element)
-        console.log(element.children())
+        console.log(element[4])
     }
 }
 eventSource.on(event_types.CHAT_CHANGED, onCharAdvChanged);
