ba6efc4266b64472ef3b695ba6c19e8b2a51eddc
SUCC-u-lent <william.i.ney@gmail.com>
2024-10-09 22:26:07 +1000

    Commited Change

--- advdesccfg/entry.js
+++ advdesccfg/entry.js
@@ -6,11 +6,14 @@ import {
     event_types,
     eventSource, 
 } from "../../../../../script.js";
-return function init(){
+function init(){
     jQuery(async()=>{
         if (typeof(extension_settings[extensionName]) == "undefined")
         {extension_settings[extensionName]={}; saveSettingsDebounced()}
         const element = await $.get(`${extensionFolderPath}/advdesccfg/owner.html`);
         $("#character_popup").children().eq(4).after(element)
     })
+}
+return {
+    init:init
 }
