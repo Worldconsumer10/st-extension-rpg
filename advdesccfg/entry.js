870fc8e858e3422ccd15e8eb360029229acc0be6
SUCC-u-lent <william.i.ney@gmail.com>
2024-10-09 22:23:12 +1000

    Commited Change

---
+++ advdesccfg/entry.js
@@ -0,0 +1,16 @@
+const extensionName = "st-extension-rpg";
+const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+import { extension_settings, getContext } from "../../../../extensions.js";
+import { 
+    saveSettingsDebounced,
+    event_types,
+    eventSource, 
+} from "../../../../../script.js";
+return function init(){
+    jQuery(async()=>{
+        if (typeof(extension_settings[extensionName]) == "undefined")
+        {extension_settings[extensionName]={}; saveSettingsDebounced()}
+        const element = await $.get(`${extensionFolderPath}/advdesccfg/owner.html`);
+        $("#character_popup").children().eq(4).after(element)
+    })
+}
