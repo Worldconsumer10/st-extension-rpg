0633e1e18f534c83a99b2e29878793d925d0cd20
SUCC-u-lent <william.i.ney@gmail.com>
2024-10-24 21:57:29 +1000

    Commited Change

---
+++ chatdesc/chat_ui_adder.js
@@ -0,0 +1,27 @@
+const extensionName = "st-extension-rpg";
+const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+import { extension_settings, getContext } from "../../../../extensions.js";
+import { 
+    saveSettingsDebounced,
+    event_types,
+    eventSource, 
+} from "../../../../../script.js";
+
+eventSource.on(event_types.CHAT_CHANGED,function(){
+    var messages = $('.mes')
+    const user = getContext().name1
+    const ai = getContext().name2
+    messages.forEach(element => {
+        console.log(element)
+    });
+})
+
+function init(){
+    jQuery(async()=>{
+        if (typeof(extension_settings[extensionName]) == "undefined")
+        {extension_settings[extensionName]={}; saveSettingsDebounced()}
+    })
+}
+export {
+    init
+}
