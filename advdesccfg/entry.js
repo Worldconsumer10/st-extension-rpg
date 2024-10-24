e3e32d1f327313393aab0ca6767df45d65f9ef2d
SUCC-u-lent <william.i.ney@gmail.com>
2024-10-25 00:10:13 +1000

    Commited Change

--- advdesccfg/entry.js
+++ advdesccfg/entry.js
@@ -19,70 +19,81 @@ eventSource.on(event_types.CHAT_CHANGED,function(){
 
 function addAdvDesc(){
     jQuery(async () => {
-        // Select the element to observe
-        const targetElement = document.getElementById('your_name');
-
-        // Create a new observer instance
-        const observer = new MutationObserver(function(mutationsList) {
-            for (let mutation of mutationsList) {
-                console.log(mutation)
-                if (mutation.type == 'attributes' && mutation.attributeName == 'value') {
-                    $("#rpg_topcontent_tab").empty()
-                    if (
-                        typeof(extension_settings[extensionName]["attributes"]) !== "undefined" &&
-                        typeof(extension_settings[extensionName]["attributes"][getUserName()]) !== "undefined"
-                    ) {
-                        const username = getUserName();
-                        const userAttributes = extension_settings[extensionName]["attributes"][username];
-                    
-                        for (const key in userAttributes) {
-                            if (Object.prototype.hasOwnProperty.call(userAttributes, key)) {
-                                const element = userAttributes[key]; // 'element' is the value of the attribute
-                    
-                                // Ensure that the key and element are valid before proceeding
-                                if (key && element) {
-                                    const newAttributeElement = $(attributeElementTemplate).clone();
-                    
-                                    // Append the cloned attribute element to the tab
-                                    $("#rpg_topcontent_tab").append(newAttributeElement);
-                    
-                                    // Set the values of the attribute elements based on the stored data
-                                    newAttributeElement.find("#att_name").val(key);    // Set the attribute name (key)
-                                    newAttributeElement.find("#att_val").val(element); // Set the attribute value (element)
-                                    newAttributeElement.find("#att_saved").text("🔵 Loaded");
-                    
-                                    // Remove button functionality
-                                    newAttributeElement.find("#removeButton").on("click", function () {
-                                        newAttributeElement.remove();
-                    
-                                        // Ensure the settings structure is initialized before removal
-                                        if (typeof(extension_settings[extensionName]["attributes"]) == "undefined") {
-                                            extension_settings[extensionName]["attributes"] = {};
-                                        }
-                                        if (typeof(extension_settings[extensionName]["attributes"][username]) == "undefined") {
-                                            extension_settings[extensionName]["attributes"][username] = {};
-                                        }
-                    
-                                        // Remove the attribute from the settings
-                                        delete extension_settings[extensionName]["attributes"][username][key];
-                    
-                                        // Save the updated settings
-                                        saveSettingsDebounced();
-                                    });
-                    
-                                    // Remove the save button since this attribute is already loaded
-                                    newAttributeElement.find("#saveButton").remove();
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        var targetNodes = $("#your_name");
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        var myObserver = new MutationObserver(mutationHandler);
+        var obsConfig = {
+          childList: true,
+          characterData: true,
+          attributes: true,
+          subtree: true
+        };
+        
+        //--- Add a target node to the observer. Can only add one node at a time.
+        targetNodes.each(function() {
+          myObserver.observe(this, obsConfig);
         });
-
-        // Start observing the target element for attribute changes
-        observer.observe(targetElement, { attributes: true });
-
+        
+        function mutationHandler(mutationRecords) {
+          console.info("mutationHandler:");
+        
+          mutationRecords.forEach(function(mutation) {
+            console.log(mutation);
+        
+            if (typeof mutation.removedNodes == "object") {
+              var jq = $(mutation.removedNodes);
+            //   $("#rpg_topcontent_tab").empty()
+            //   if (
+            //       typeof(extension_settings[extensionName]["attributes"]) !== "undefined" &&
+            //       typeof(extension_settings[extensionName]["attributes"][getUserName()]) !== "undefined"
+            //   ) {
+            //       const username = getUserName();
+            //       const userAttributes = extension_settings[extensionName]["attributes"][username];
+              
+            //       for (const key in userAttributes) {
+            //           if (Object.prototype.hasOwnProperty.call(userAttributes, key)) {
+            //               const element = userAttributes[key]; // 'element' is the value of the attribute
+              
+            //               // Ensure that the key and element are valid before proceeding
+            //               if (key && element) {
+            //                   const newAttributeElement = $(attributeElementTemplate).clone();
+              
+            //                   // Append the cloned attribute element to the tab
+            //                   $("#rpg_topcontent_tab").append(newAttributeElement);
+              
+            //                   // Set the values of the attribute elements based on the stored data
+            //                   newAttributeElement.find("#att_name").val(key);    // Set the attribute name (key)
+            //                   newAttributeElement.find("#att_val").val(element); // Set the attribute value (element)
+            //                   newAttributeElement.find("#att_saved").text("🔵 Loaded");
+              
+            //                   // Remove button functionality
+            //                   newAttributeElement.find("#removeButton").on("click", function () {
+            //                       newAttributeElement.remove();
+              
+            //                       // Ensure the settings structure is initialized before removal
+            //                       if (typeof(extension_settings[extensionName]["attributes"]) == "undefined") {
+            //                           extension_settings[extensionName]["attributes"] = {};
+            //                       }
+            //                       if (typeof(extension_settings[extensionName]["attributes"][username]) == "undefined") {
+            //                           extension_settings[extensionName]["attributes"][username] = {};
+            //                       }
+              
+            //                       // Remove the attribute from the settings
+            //                       delete extension_settings[extensionName]["attributes"][username][key];
+              
+            //                       // Save the updated settings
+            //                       saveSettingsDebounced();
+            //                   });
+              
+            //                   // Remove the save button since this attribute is already loaded
+            //                   newAttributeElement.find("#saveButton").remove();
+            //               }
+            //           }
+            //       }
+            //   }
+            }
+          });
+        }
 
         if (typeof(extension_settings[extensionName]) == "undefined") {
             extension_settings[extensionName] = {}; 
