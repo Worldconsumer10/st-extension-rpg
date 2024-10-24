88e4323e4484a263ff7bd244e67aba5be8cf6cd4
SUCC-u-lent <william.i.ney@gmail.com>
2024-10-25 00:00:46 +1000

    Commited Change

--- advdesccfg/entry.js
+++ advdesccfg/entry.js
@@ -17,7 +17,73 @@ eventSource.on(event_types.CHAT_CHANGED,function(){
     }
 })
 
+function onPlayerChanged(){
+    // Select the element to observe
+    const targetElement = document.getElementById('your_name');
+
+    // Create a new observer instance
+    const observer = new MutationObserver(function(mutationsList) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'value') {
+                $("#rpg_topcontent_tab").empty()
+                if (
+                    typeof(extension_settings[extensionName]["attributes"]) !== "undefined" &&
+                    typeof(extension_settings[extensionName]["attributes"][getUserName()]) !== "undefined"
+                ) {
+                    const username = getUserName();
+                    const userAttributes = extension_settings[extensionName]["attributes"][username];
+                
+                    for (const key in userAttributes) {
+                        if (Object.prototype.hasOwnProperty.call(userAttributes, key)) {
+                            const element = userAttributes[key]; // 'element' is the value of the attribute
+                
+                            // Ensure that the key and element are valid before proceeding
+                            if (key && element) {
+                                const newAttributeElement = $(attributeElementTemplate).clone();
+                
+                                // Append the cloned attribute element to the tab
+                                $("#rpg_topcontent_tab").append(newAttributeElement);
+                
+                                // Set the values of the attribute elements based on the stored data
+                                newAttributeElement.find("#att_name").val(key);    // Set the attribute name (key)
+                                newAttributeElement.find("#att_val").val(element); // Set the attribute value (element)
+                                newAttributeElement.find("#att_saved").text("🔵 Loaded");
+                
+                                // Remove button functionality
+                                newAttributeElement.find("#removeButton").on("click", function () {
+                                    newAttributeElement.remove();
+                
+                                    // Ensure the settings structure is initialized before removal
+                                    if (typeof(extension_settings[extensionName]["attributes"]) == "undefined") {
+                                        extension_settings[extensionName]["attributes"] = {};
+                                    }
+                                    if (typeof(extension_settings[extensionName]["attributes"][username]) == "undefined") {
+                                        extension_settings[extensionName]["attributes"][username] = {};
+                                    }
+                
+                                    // Remove the attribute from the settings
+                                    delete extension_settings[extensionName]["attributes"][username][key];
+                
+                                    // Save the updated settings
+                                    saveSettingsDebounced();
+                                });
+                
+                                // Remove the save button since this attribute is already loaded
+                                newAttributeElement.find("#saveButton").remove();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    // Start observing the target element for attribute changes
+    observer.observe(targetElement, { attributes: true });
+}
+
 function addAdvDesc(){
+    onPlayerChanged()
     jQuery(async () => {
         if (typeof(extension_settings[extensionName]) == "undefined") {
             extension_settings[extensionName] = {}; 
@@ -60,10 +126,10 @@ function addAdvDesc(){
                             newAttributeElement.remove();
         
                             // Ensure the settings structure is initialized before removal
-                            if (typeof(extension_settings[extensionName]["attributes"]) === "undefined") {
+                            if (typeof(extension_settings[extensionName]["attributes"]) == "undefined") {
                                 extension_settings[extensionName]["attributes"] = {};
                             }
-                            if (typeof(extension_settings[extensionName]["attributes"][username]) === "undefined") {
+                            if (typeof(extension_settings[extensionName]["attributes"][username]) == "undefined") {
                                 extension_settings[extensionName]["attributes"][username] = {};
                             }
         
@@ -87,6 +153,7 @@ function addAdvDesc(){
         
             newAttributeElement.find("#removeButton").on("click", function() {
                 newAttributeElement.remove();
+                const username = getUserName()
                 
                 // Ensure the settings structure is initialized before removal
                 if (typeof(extension_settings[extensionName]["attributes"]) === "undefined") {
@@ -95,9 +162,10 @@ function addAdvDesc(){
                 if (typeof(extension_settings[extensionName]["attributes"][username]) === "undefined") {
                     extension_settings[extensionName]["attributes"][username] = {};
                 }
+                const attributeName = newAttributeElement.find("#att_name").val()
                 
                 // Remove the attribute from the settings
-                delete extension_settings[extensionName]["attributes"][username][key];
+                delete extension_settings[extensionName]["attributes"][username][attributeName];
                 
                 // Save the updated settings
                 saveSettingsDebounced();
@@ -111,7 +179,6 @@ function addAdvDesc(){
                 {extension_settings[extensionName]["attributes"][username]={}}
                 const attributeName = newAttributeElement.find("#att_name").val()
                 const attributeValue = newAttributeElement.find("#att_val").val()
-                console.log(attributeName,attributeValue)
 
                 extension_settings[extensionName]["attributes"][username][attributeName] = attributeValue
                 saveSettingsDebounced()
